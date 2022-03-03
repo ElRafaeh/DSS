630fcf085a519d9f8603de4377f2dc356c63aae7
jdgl3 <jdgl3@alu.ua.es>
2022-03-03 19:27:20 +0100

    Primeros pasos

---
+++ sharyo/app/Models/Trip.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+    protected $table = 'trips';
+    protected $fillable = [
+        'tripID',
+        'destination',
+        'origin',
+        'date',
+        'distance',
+        'availableSeats'
+    ];
+
+    protected $hidden = [
+        'create_at', 
+        'updated_at',
+    ];
+
+
+}
