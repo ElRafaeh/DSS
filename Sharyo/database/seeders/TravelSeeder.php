0a6414062b01b4341976f7cc9df10b4727ca6af9
Jesus <jpo24@alu.ua.es>
2022-05-28 18:04:42 +0200

    Rellenados seeders

--- Sharyo/database/seeders/TravelSeeder.php
+++ Sharyo/database/seeders/TravelSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+
 
 class TravelSeeder extends Seeder
 {
@@ -13,6 +16,21 @@ class TravelSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('trip_user')->insert([
+            'user_email' => 'jesus@gmail.com',   
+            'trip_id' => '1' 
+        ]);
+
+        DB::table('trip_user')->insert([
+            'user_email' => 'jesus@gmail.com',   
+            'trip_id' => '2' 
+        ]);
+
+        DB::table('trip_user')->insert([
+            'user_email' => 'jesus@gmail.com',   
+            'trip_id' => '4' 
+        ]);
+
+        
     }
 }
--- Sharyo/database/seeders/TripSeeder.php
+++ Sharyo/database/seeders/TripSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TripSeeder extends Seeder
 {
@@ -13,6 +14,69 @@ class TripSeeder extends Seeder
      */
     public function run()
     {
-  //
+      DB::table('trips')->insert([
+        'date' => '29/05/2022',
+        'availableSeats' => '3',   
+        'driver' => 'Y1981204C',
+        'origin' => 'Alicante',
+        'destination' => 'Valencia',
+        'price' => '18'
+      ]);
+
+      DB::table('trips')->insert([
+        'date' => '03/06/2022',
+        'availableSeats' => '1',   
+        'driver' => '49627353T',
+        'origin' => 'Jaén',
+        'destination' => 'Pozo Alcón',
+        'price' => '21'
+      ]);
+
+      DB::table('trips')->insert([
+        'date' => '29/05/2022',
+        'availableSeats' => '3',   
+        'driver' => '90865644N',
+        'origin' => 'Alicante',
+        'destination' => 'Valencia',
+        'price' => '23'
+      ]);
+
+      DB::table('trips')->insert([
+        'date' => '29/05/2022',
+        'availableSeats' => '3',   
+        'driver' => 'X2846345C',
+        'origin' => 'Alicante',
+        'destination' => 'Valencia',
+        'price' => '27'
+      ]);
+
+      DB::table('trips')->insert([
+        'date' => '02/06/2022',
+        'availableSeats' => '2',   
+        'driver' => '49627353T',
+        'origin' => 'Jaén',
+        'destination' => 'Alicante',
+        'price' => '9'
+      ]);
+
+      DB::table('trips')->insert([
+        'date' => '29/05/2022',
+        'availableSeats' => '3',   
+        'driver' => '90865644N',
+        'origin' => 'Valencia',
+        'destination' => 'Alicante',
+        'price' => '30'
+      ]);
+
+      DB::table('trips')->insert([
+        'date' => '29/05/2022',
+        'availableSeats' => '3',   
+        'driver' => '90865644N',
+        'origin' => 'Albacete',
+        'destination' => 'Jaén',
+        'price' => '10'
+      ]);
+
+  
     }
 }
